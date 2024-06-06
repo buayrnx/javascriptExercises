d97d15c8da3e989f1b8062379a3510ee594a2de5
Buse Ayrancıoğlu <ayrancioglu22@itu.edu.tr>
2024-06-06 16:16:46 +0300

    Adding solutions of tasks about variables.

---
+++ conditions.js
@@ -0,0 +1,33 @@
+//TASK1
+const number1 = prompt("Enter first number:");
+const number2 = prompt("Enter second number:");
+const subtraction = number1 - number2;
+console.log(`The subtraction of ${number2} from ${number1} is ${subtraction}.`);
+const multiply = number1 * number2;
+console.log(`The multiplication of ${number1} and ${number2} is ${multiply}.`);
+const division = number1 / number2;
+console.log(`The division of ${number1} by ${number2} is ${division}.`);
+
+//TASK2
+const firstName = prompt("Enter your name:");
+console.log(`Merhaba, ${firstName}!`);
+console.log(firstName.length);
+
+//TASK3
+const numberInput1 = prompt("Enter a number:");
+if (numberInput1 > 0) {
+  console.log("The number is positive.");
+} else if (numberInput1 < 0) {
+  console.log("The number is zero.");
+} else {
+  console.log("The number is negative.");
+}
+
+const numberInput2 = prompt("Enter another number:");
+if (numberInput1 > numberInput2) {
+  console.log(`${numberInput1} is bigger than the ${numberInput2}`);
+} else if (numberInput1 < numberInput2) {
+  console.log(`${numberInput1} is smaller than the ${numberInput2}`);
+} else {
+  console.log(`${numberInput1} is equal to the ${numberInput2}`);
+}
